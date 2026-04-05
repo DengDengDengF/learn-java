79cdbdc269960f7d84e3032d4305198338ef2aaa
ldf <lidengfenglidengfeng4@gmail.com>
2026-04-05 13:04:56 +0800

    正则表达式

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        String re2 = "[0-9a-fA-F]{6}";
+        System.out.println("abcdef".matches(re2));
     }
 }
