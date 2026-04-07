206528b3d62494b1c15cb8a52533f3473d6f45bf
ldf <lidengfenglidengfeng4@gmail.com>
2026-04-07 21:04:55 +0800

    正则括号

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.*;
 
 public class Main {
     public static void main(String[] args) {
-        String re2 = "[0-9a-fA-F]{6}";
-        System.out.println("abcdef".matches(re2));
+        String re2 = "learn\\s(Java|Php|Go)";
+        System.out.println("learn Java".matches(re2));
     }
 }
