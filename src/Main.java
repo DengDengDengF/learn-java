aa542f90311c8514318ef7d7e72f12845b577f0b
ldf <lidengfenglidengfeng4@gmail.com>
2026-04-16 20:26:02 +0800

    搜索替换

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,8 @@ import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        Pattern pattern = Pattern.compile("(\\d+?)(0*)");
-        Matcher matcher = pattern.matcher("1230000");
-        if (matcher.matches()) {
-            System.out.println("group1=" + matcher.group(1)); // "1230000"
-            System.out.println("group2=" + matcher.group(2)); // ""
-        }
+        String s = "the quick brown fox jumps over the lazy dog.";
+        String r = s.replaceAll("\\s([a-z]{3})\\s", " <b>$1</b> ");
+        System.out.println(r);
     }
 }
